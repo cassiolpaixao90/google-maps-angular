5176096435db4540b0514bd5d4e9d1d79d5fd8ee
Charlie McBride <mcbride.charles4@gmail.com>
2016-03-20 12:41:21 -0500

    First commit

---
+++ src/angular-google-map.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+angular
+.factory('GoogleMap', googleMapFactory)
+.directive('googleMap', googleMapDirective);
+
+function googleMapFactory() {
+
+}
+
+function googleMapDirective() {
+
+}
+
+})();
